851bbf8118cccef3f9755ee502a91199624dfca8
kongkongyzt <kongkongyzt@gmail.com>
2014-08-16 12:04:20 +0800

    大量bug修复

--- framework/common/application.class.php
+++ framework/common/application.class.php
@@ -4,10 +4,13 @@
     
     var $route;
     var $handler;
+    var $config;
     var $params=array();
     var $dbconnection;
     
     function __construct(){
+        global $CONFIG;
+        $this->config=$CONFIG;
         $db=new db();
         $this->dbconnection=$db->connection;
     }
@@ -15,12 +18,11 @@
     function checkRoute($requestURL){
         if(array_key_exists($requestURL,$this->route)){
             $mapping=$this->route[$requestURL];
-            $controller=explode('.',$mapping)[0];
-            $action=explode('.',$mapping)[1];
-            
+            $controller=explode('->',$mapping)[0];
+            $action=explode('->',$mapping)[1];
             //check the existance of controller
-            if(!file_exists(CTL_PATH.$controller.'.class.php')){
-                if($config['DEBUG']){
+            if(!file_exists(CTL_PATH.$controller.'.ctl.php')){
+                if($this->config['DEBUG']){
                     die('控制器'.$controller.'不存在');
                 }else{
                     die('404');
@@ -28,10 +30,10 @@
             }
             
             //check the existance of method
-            $this->$handler=array(CTL_PATH.$controller.'.class.php',$action);
+            $this->handler=array(CTL_PATH.$controller.'.ctl.php',$action);
             if(!is_callable($handler)){
-                if($config['DEBUG']){
-                    die('控制器'.$controller.'不存在'.$action.'方法');
+                if($this->config['DEBUG']){
+                    die('控制器'.$controller.'存在,但是不存在'.$action.'方法');
                 }else{
                     die('404');
                 }
