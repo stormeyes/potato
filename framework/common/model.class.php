150bac1be85336ba8b358195db958f4be0ea87d3
kongkongyzt on local ubuntu <kongkongyzt@gmail.com>
2014-10-01 21:52:57 +0800

    model层优化

--- framework/common/model.class.php
+++ framework/common/model.class.php
@@ -1,15 +1,9 @@
 <?php
     class BASE_MDL{
-    
-        var $dbconnection;
-        var $config;
         var $db;
-        
+
         function __construct(){
-            global $CONFIG;
-            $this->config=$CONFIG;
             $this->db=new db();
-            $this->dbconnection=$this->db->connection;
         }
     }
 ?>
--- site/model/user.model.php
+++ site/model/user.model.php
@@ -3,10 +3,6 @@
         var $id;
         var $username;
         var $password;
-        
-        function __construct(){
-            parent::__construct();
-        }
 
         function getUserByName($username){
             var_dump($this->db->query('select * from user','array'));
