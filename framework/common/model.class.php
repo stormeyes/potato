5227e2c6b43fd05b2996ef535bd0e7122b78532c
kongkongyzt on local ubuntu <kongkongyzt@gmail.com>
2014-10-02 10:42:12 +0800

    完善ORM中的get函数

--- framework/common/model.class.php
+++ framework/common/model.class.php
@@ -5,5 +5,34 @@
         function __construct(){
             $this->db=new db();
         }
+        
+        function get($condition){
+            var_dump($this->db->query('select * from '.$this->tablename.' where '.$condition,'array'));
+            //echo $this->tablename;
+        }
+        
+        function insert(){
+            echo '<br>===============</br>';
+            $attr=get_object_vars($this);
+            unset($attr['db']);
+            unset($attr['tablename']);
+            $s1='';
+            $s2='';
+            foreach($attr as $key=>$value){
+                $s1=$s1.$key.',';
+                $s2=$s2.$value.',';
+            }
+            $insert="insert into ".$this->tablename."(".$s1.") values(".$s2.")";
+            echo $insert;
+            
+        }
+        
+        function update(){
+        
+        }
+        
+        function delete(){
+        
+        }
     }
 ?>
--- site/model/user.model.php
+++ site/model/user.model.php
@@ -1,11 +1,17 @@
 <?php
     class user_model extends BASE_MDL{
+        var $tablename='user';
         var $id;
         var $username;
         var $password;
 
         function getUserByName($username){
-            var_dump($this->db->query('select * from user','array'));
+            //var_dump($this->db->query('select * from user','array'));
+            $this->get('id=1',$this);
+            $this->id=2;
+            $this->username='hahaha';
+            $this->password='fuckman';
+            $this->insert();
         }
     }
 ?>
